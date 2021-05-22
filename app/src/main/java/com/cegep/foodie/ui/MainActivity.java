05ae581d4a71748051108a9733af0c8e02ba6ce9
Himansh Batra <batrah91@gmail.com>
2021-05-22 00:25:44 -0400

    Implement sign out

--- app/src/main/java/com/cegep/foodie/ui/MainActivity.java
+++ app/src/main/java/com/cegep/foodie/ui/MainActivity.java
@@ -2,12 +2,15 @@ package com.cegep.foodie.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-
 import com.cegep.foodie.R;
+import com.cegep.foodie.ui.auth.AuthActivity;
 import com.cegep.foodie.ui.createrecipe.CreateRecipeActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.home);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.logout) {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
         Fragment fragment = new HomeFragment();
-
-
-
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, fragment, fragment.getClass().getSimpleName()).commit();
